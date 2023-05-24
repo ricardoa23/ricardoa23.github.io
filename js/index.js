095aab38cac0298112903ac95021ea08047cb064
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-05-24 15:08:17 -0500

    added js and animated Lottie JSON still needs to be positioned + and added index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-animation'), // the HTML element that you want to animate
+    renderer: 'svg', // 'svg', 'canvas', 'html'
+    loop: true, // if the animation should loop
+    autoplay: true, // if the animation should autoplay
+    path: 'json/Mail.json' // the path to the animation JSON
+  });
+  
