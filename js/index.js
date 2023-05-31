0d0f1fbb646f5ccac327b6e6db5c0c62ef41d0ab
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-05-31 17:41:24 -0500

    working on js changes for event listener

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
 let animation = lottie.loadAnimation({
-    container: document.getElementById('lottie-animation'), // the HTML element that you want to animate
-    renderer: 'svg', // 'svg', 'canvas', 'html'
-    loop: true, // if the animation should loop
-    autoplay: true, // if the animation should autoplay
-    path: 'json/Mail-light.json' // the path to the animation JSON
+  container: document.getElementById("lottie-animation"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "json/Mail-light.json",
+});
+
+document
+  .querySelector('#darkmode-toggle')
+  .addEventListener("change", function () {
+    if (this.checked) {
+      document.querySelector("body").style.backgroundColor = "gray";
+      document.querySelector("main").style.backgroundColor = "#00003f";
+      document.querySelector("li").style.color = "dodgerblue";
+      document.querySelector("h1").style.color = "dodgerblue";
+      document.querySelector("*").style.color = "rgba(128,128,128, 0.7)";
+
+    } else {
+      document.querySelector("body").style.backgroundColor = "lightgray";
+      document.querySelector("main").style.backgroundColor = "white";
+      document.querySelector("h1").style.color = "#AA0000";
+      document.querySelector("*").style.color = "unset";
+    }
   });
-  
+
+  // const darkmode = () =>  {
+  //   document.querySelector("body").backgroundColor = "black";
+  //   document.querySelector("h1").color = "dogerblue";
+  //   document.querySelector("*").color = "lightgray";
+  // }
