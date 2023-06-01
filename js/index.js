cb4010ea72795d5573484d372372d646931244fa
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-06-01 18:06:11 -0500

    more changes to dark mode made still some left

--- js/index.js
+++ js/index.js
@@ -1,31 +1,57 @@
-let animation = lottie.loadAnimation({
-  container: document.getElementById("lottie-animation"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "json/Mail-light.json",
-});
+let animation;
+
+function loadAnimation(path) {
+  animation = lottie.loadAnimation({
+    container: document.getElementById("lottie-animation"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: path,
+  });
+}
+
+loadAnimation("json/Mail-light.json");
 
 document
-  .querySelector('#darkmode-toggle')
+  .querySelector("#darkmode-toggle")
   .addEventListener("change", function () {
+    if (animation) {
+      animation.destroy();
+    }
+
     if (this.checked) {
+      loadAnimation("json/Mail-dark.json");
       document.querySelector("body").style.backgroundColor = "gray";
       document.querySelector("main").style.backgroundColor = "#00003f";
-      document.querySelector("li").style.color = "dodgerblue";
+      for (let li of document.querySelectorAll("li")) {
+        li.classList.add("darkmode");
+      }
+      for (let li of document.querySelectorAll("section")) {
+        li.style.color = "rgba(128,128,128, 0.75)";
+      }
+      for (let li of document.querySelectorAll("h2")) {
+        li.style.color = "rgba(128,128,128, 0.75)";
+      }
       document.querySelector("h1").style.color = "dodgerblue";
-      document.querySelector("*").style.color = "rgba(128,128,128, 0.7)";
-
     } else {
+      loadAnimation("json/Mail-light.json");
       document.querySelector("body").style.backgroundColor = "lightgray";
       document.querySelector("main").style.backgroundColor = "white";
+      for (let li of document.querySelectorAll("li")) {
+        li.classList.remove("darkmode");
+      }
       document.querySelector("h1").style.color = "#AA0000";
-      document.querySelector("*").style.color = "unset";
+      for (let li of document.querySelectorAll("section")) {
+        li.style.color = "unset";
+      }
+      for (let li of document.querySelectorAll("h2")) {
+        li.style.color = "unset";
+      }
     }
   });
 
-  // const darkmode = () =>  {
-  //   document.querySelector("body").backgroundColor = "black";
-  //   document.querySelector("h1").color = "dogerblue";
-  //   document.querySelector("*").color = "lightgray";
-  // }
+// const darkmode = () =>  {
+//   document.querySelector("body").backgroundColor = "black";
+//   document.querySelector("h1").color = "dogerblue";
+//   document.querySelector("*").color = "lightgray";
+// } rgba(128,128,128, 0.75)
